90b201b86b72b6ed37c9a5e8d14440ae1a1ee787
Nel Nawe <hornela.nawessi@yahoo.com>
2021-07-02 22:24:53 +0200

    my first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./components";
+import App from "./App";
+import "milligram";
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
+
+
